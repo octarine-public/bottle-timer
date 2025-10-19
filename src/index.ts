45469dce9c9cdc6e73416e6ee961704799f83388
SicdeX <sicdex@gmail.com>
2025-10-19 22:10:13 +0600

    add: new script

---
+++ src/index.ts
@@ -0,0 +1,74 @@
+import "./translations"
+
+import {
+	Color,
+	DOTA_RUNES,
+	DOTAGameState,
+	DOTAGameUIState,
+	EventsSDK,
+	GameRules,
+	GameState,
+	GUIInfo,
+	InputManager,
+	item_bottle,
+	RendererSDK,
+	TextFlags
+} from "github.com/octarine-public/wrapper/index"
+
+import { MenuManager } from "./menu"
+
+new (class CBottleTimer {
+	private readonly menu!: MenuManager
+
+	constructor(canBeInitialized: boolean) {
+		if (!canBeInitialized) {
+			return
+		}
+		this.menu = new MenuManager()
+		EventsSDK.on("Draw", this.Draw.bind(this))
+	}
+
+	private get isUIGame() {
+		return GameState.UIState === DOTAGameUIState.DOTA_GAME_UI_DOTA_INGAME
+	}
+	private get isPostGame() {
+		return (
+			GameRules === undefined ||
+			GameRules.GameState === DOTAGameState.DOTA_GAMERULES_STATE_POST_GAME
+		)
+	}
+	private get shouldDraw() {
+		return this.menu.State.value && this.isUIGame && !this.isPostGame
+	}
+	protected Draw() {
+		if (!this.shouldDraw) {
+			return
+		}
+		const entity = InputManager.SelectedUnit
+		if (entity === undefined || entity.IsIllusion) {
+			return
+		}
+		const bottle = entity.GetItemByClass(item_bottle)
+		if (bottle === undefined || bottle.StoredRune === DOTA_RUNES.DOTA_RUNE_INVALID) {
+			return
+		}
+		const remainingTime = bottle.RuneExpireTime
+		if (remainingTime <= 0) {
+			return
+		}
+		const slot = entity.Inventory.GetItemSlot(bottle)
+		if (slot === undefined) {
+			return
+		}
+		const position = GUIInfo.GetLowerHUDForUnit(entity).MainInventorySlots[slot],
+			text = remainingTime.toFixed(remainingTime < 1 ? 1 : 0)
+		RendererSDK.TextByFlags(
+			text,
+			position,
+			Color.White,
+			this.menu.TextSize,
+			TextFlags.Center,
+			600
+		)
+	}
+})(true)
---
+++ src/menu.ts
@@ -0,0 +1,30 @@
+import { ImageData, Menu } from "github.com/octarine-public/wrapper/index"
+
+export class MenuManager {
+	public readonly State: Menu.Toggle
+
+	private readonly sizeScale = 3
+	private readonly textSize: Menu.Slider
+
+	private readonly tree = Menu.AddEntry("Visual")
+	private readonly node = this.tree.AddNode(
+		"Bottle rune timer",
+		ImageData.GetItemTexture("item_bottle"),
+		"Shows the time when the rune will be activated",
+		0
+	)
+
+	constructor() {
+		this.State = this.node.AddToggle("State", true)
+		this.textSize = this.node.AddSlider("Additional text size", 0, 0, 100)
+	}
+	public get TextSize() {
+		return Math.remapRange(
+			this.textSize.value,
+			this.textSize.min,
+			this.textSize.max,
+			this.sizeScale,
+			this.sizeScale / 2
+		)
+	}
+}
